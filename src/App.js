7165efcfc332dd072acd826e6067638427146a08
Hayden Kiltoff <hayden060@gmail.com>
2022-05-29 14:45:30 -0700

    stucture building

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Body from './components/body';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Body />
+    
+     
+    </div>
+  );
+}
+
+export default App;
